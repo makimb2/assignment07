0fea5c982e1b88e92249ca32d5da26d11eca7ce1
matthew kimball <matthewakimball@gmail.com>
2015-06-24 09:31:28 -0600

    final update assignment 7
    update

---
+++ Demos/Target.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPages/Frontend.master" AutoEventWireup="true" CodeFile="Target.aspx.cs" Inherits="$relurlnamespa
+ce$_Target" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cpMainContent" Runat="Server">
+</asp:Content>
+
--- Demos/Target.aspx.cs
+++ Demos/Target.aspx.cs
@@ -4,15 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Web.UI.MasterPage;
 
-public partial class MasterPages_Frontend : BasePage
+public partial class Demos_Target: BasePage
 {
     protected void Page_Load(object sender, EventArgs e)
-    {
-
-    }
-    protected void Menu1_MenuItemClick(object sender, MenuEventArgs e)
     {
 
     }
