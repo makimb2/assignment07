823b9ca3cd7741fdd17c93e17cb84ca0b0ce9fad
makimb2 <matthewakimball@gmail.com>
2015-06-23 22:52:58 -0600

    update
    update

---
+++ App_Code/RouteConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for RouteConfig
+/// </summary>
+public class RouteConfig
+{
+	public RouteConfig()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
--- MasterPages/Frontend.master.cs
+++ MasterPages/Frontend.master.cs
@@ -9,7 +9,17 @@ public partial class MasterPages_Frontend : BasePage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        switch (Page.Theme.ToLower())
+        {
+            case "darkgrey":
+                Menu1.Visible = false;
+                TreeView1.Visible = true;
+                break;
+            default:
+                Menu1.Visible = true;
+                TreeView1.Visible = false;
+                break;
+        }
     }
     protected void Menu1_MenuItemClick(object sender, MenuEventArgs e)
     {
